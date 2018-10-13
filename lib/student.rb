7cae71d388c3a2937b39872e553577d699205b98
github username <slonanuga@gmail.com>
2018-10-13 18:50:54 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+def initialize
+    @knowledge = []
+end
+
+def learn(facts)
+   @knowledge << facts
+end
+
+def knowledge
+@knowledge
+end
+
+
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,20 @@
+class User
+
+    #user is parent class. initialize with first+last name.
+
+    attr_accessor :first_name, :last_name
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+end
